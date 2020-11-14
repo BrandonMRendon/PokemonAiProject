b681608f122351a6441b86af4cf5ea18da5ab6bc
ireichard <ireichard4@gmail.com>
2020-11-14 03:30:30 -0800

    Added updated PokeDex.csv with stats and fixed PokeDex.py to work with new stats.

--- PokeDex.py
+++ PokeDex.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 class Pokemon:
-  def __init__(self,name,type,move1,move2,move3,move4,move1type,move2type,move3type,move4type):
+  def __init__(self,name,type,move1,move2,move3,move4,move1type,move2type,move3type,move4type,hp_stat,atk_stat,def_stat,spec_stat,speed_stat):
     self.name = name
     self.type = type
     self.move1 = move1
@@ -12,30 +12,44 @@ class Pokemon:
     self.move2type = move2type
     self.move3type = move3type
     self.move4type = move4type
+    self.hp_stat = hp_stat
+    self.atk_stat = atk_stat
+    self.def_stat = def_stat
+    self.spec_stat = spec_stat
+    self.speed_stat = speed_stat
+
+
 def printPoke(Pokemon):
     print(Pokemon.name + " ~ " + Pokemon.type + "\n")
     print(Pokemon.move1 + " ~ " + Pokemon.move1type + "\n")
     print(Pokemon.move2 + " ~ " + Pokemon.move2type + "\n")
     print(Pokemon.move3 + " ~ " + Pokemon.move3type + "\n")
-    print(Pokemon.move4 + " ~ " + Pokemon.move4type + "\n\n\n")
+    print(Pokemon.move4 + " ~ " + Pokemon.move4type + "\n")
+    print("Stats= HP:" + str(Pokemon.hp_stat)
+          + " ATK:" + str(Pokemon.atk_stat)
+          + " DEF:" + str(Pokemon.def_stat)
+          + " SPE:" + str(Pokemon.spec_stat)
+          + " SPD:" + str(Pokemon.speed_stat) + "\n\n")
+
 
 def makeDex():
     PokeDex = []
-    df = pd.read_csv('PokeDex - Sheet1.csv')
+    df = pd.read_csv('PokeDex.csv')
     name = "Charizard"
-    index = "Name", "Types", "Move1", "Move2", "Move3", "Move4", "Move1Type", "Move2Type", "Move3Type", "Move4Type"
+    index = "Name", "Types", "Move1", "Move2", "Move3", "Move4", "Move1Type", "Move2Type", "Move3Type", "Move4Type", "HP", "Attack", "Defense", "Special", "Speed"
     print(df)
     print(df[index[0]][0])
     count1=0
-    while (count1 < 9):
+    while (count1 < 149):
         count2 = 0
         data = []
-        while (count2 < 10):
+        while (count2 < 15):
             data.append(df[index[count2]][count1])
             count2+=1
-        PokeDex.append(Pokemon(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9]))
+        PokeDex.append(Pokemon(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10],data[11],data[12],data[13],data[14]))
         count1+=1
-        printPoke(PokeDex[count1-1])
+        # printPoke(PokeDex[count1-1])
+    return PokeDex  # return value for testing purposes, sorry Brandon. -Ian
 
 if __name__ == '__main__':
     makeDex()
